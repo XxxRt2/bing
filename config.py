281c973d937e4ea55ee3e16c5bb979f1f2224c91
XxxRt <115762572+XxxRt2@users.noreply.github.com>
2024-11-13 14:15:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ API_KEY = "7749410324:AAGYPwzvt2oPzTei3Ero_68jQb9NWZqk5eI"
 
 # 预设管理员列表，可以通过其修改用户余额
 ADMINS = ["7072765311"]
+
+# 彩票 API 的基础 URL
+LOTTERY_API_URL = "https://macaumarksix.com/api/macaujc2.com"  # 请替换为实际 API 地址
