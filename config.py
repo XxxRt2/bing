df5e3747be5cfe33d4fc6c0d845930eeb8a90ea4
XxxRt <115762572+XxxRt2@users.noreply.github.com>
2024-11-13 14:13:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Telegram 机器人 API 密钥
-API_KEY = "YOUR_BOT_API_KEY"
+API_KEY = "7749410324:AAGYPwzvt2oPzTei3Ero_68jQb9NWZqk5eI"
 
 # 预设管理员列表，可以通过其修改用户余额
-ADMINS = ["admin_user_id"]
+ADMINS = ["7072765311"]
